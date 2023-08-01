b100dd041e40f36d38f66419daf8dca338d31484
Eniola Ladipo <eniolaladipo15@gmail.com>
2023-08-01 19:56:00 +0100

    First Template

---
+++ MQ.java
@@ -0,0 +1,12 @@
+package MyMathQuiz;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MQ{
+    
+    public static void main(String[] args) {
+        
+    }
+}
