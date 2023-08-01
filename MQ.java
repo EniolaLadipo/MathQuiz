012968da0e4cac607528c8596f6de9242488a0eb
Eniola Ladipo <eniolaladipo15@gmail.com>
2023-08-01 21:06:48 +0100

    Added frame with title

--- MQ.java
+++ MQ.java
@@ -6,8 +6,25 @@ import java.awt.event.*;
 
 public class MQ{
     JFrame mainFrame;
+    JPanel mainPanel;
+    JLabel label;
+
+    public void makeMainFrame(){
+        mainFrame = new JFrame("Math Quiz");
+        mainPanel = new JPanel(new GridBagLayout());
+        label = new JLabel("Welcome to the Math Quiz");
+
+        mainPanel.add(label);
+        mainFrame.add(mainPanel);
+        mainFrame.setSize(450,450);
+        mainFrame.setVisible(true);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     
+    public MQ(){
+        makeMainFrame();
+    }
     public static void main(String[] args) {
-        
+        new MQ();
     }
 }
