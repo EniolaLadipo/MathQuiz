d636a542a0fc9a7a7a08b76072c1f2194416409e
Eniola Ladipo <eniolaladipo15@gmail.com>
2023-08-01 22:32:31 +0100

    added buttons to main menu

--- MQ.java
+++ MQ.java
@@ -5,19 +5,41 @@ import java.awt.event.*;
 public class MQ{
     JFrame mainFrame;
     JPanel mainPanel;
-    JLabel label;
+    JLabel welcome;
+    JButton addition, subtraction, multiplication, division;
 
     public void makeMainPanel(){
-        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10,10,10,10);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        mainFrame = new JFrame("Math Quiz");
+        mainPanel = new JPanel(new GridBagLayout());
+        welcome = new JLabel("Welcome to the Math Quiz");
+        welcome.setFont(new Font("TimesRoman",Font.BOLD,30));
+
+        mainPanel.add(welcome,gbc);
+        gbc.gridy++;
+
+        addition = new JButton("Addition");
+        mainPanel.add(addition,gbc);
+        gbc.gridy++;
+
+        subtraction = new JButton("Subtraction");
+        mainPanel.add(subtraction,gbc);
+        gbc.gridy++;
+
+        multiplication = new JButton("Multiplication");
+        mainPanel.add(multiplication,gbc);
+        gbc.gridy++;
+
+        division = new JButton("Division");
+        mainPanel.add(division,gbc);
     }
 
     public void makeMainFrame(){
-        mainFrame = new JFrame("Math Quiz");
-        mainPanel = new JPanel(new GridBagLayout());
-        label = new JLabel("Welcome to the Math Quiz");
-        label.setFont(new Font("TimesRoman",Font.BOLD,30));
-
-        mainPanel.add(label);
+        makeMainPanel();
         mainFrame.add(mainPanel);
         mainFrame.setSize(450,450);
         mainFrame.setVisible(true);
