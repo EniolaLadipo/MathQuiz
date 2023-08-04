6afabffd5ad1935af093f2e5f0428abab556f961
Eniola Ladipo <eniolaladipo15@gmail.com>
2023-08-04 15:53:56 +0100

    Created Addition class

--- MQ.java
+++ MQ.java
@@ -3,6 +3,7 @@ import javax.swing.border.EmptyBorder;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class MQ{
     JFrame mainFrame;
@@ -10,19 +11,24 @@ public class MQ{
     JLabel welcome;
     JButton addition, subtraction, multiplication, division;
     GridBagConstraints gbc = new GridBagConstraints();
+    Random random = new Random();
 
     abstract class ArithQ{
         JPanel quizPanel, scorePanel, dataPanel;
         JButton check, retry;
         JTextField inputBox;
         JLabel mathQuestion, dataLabel;
+        int n1, n2, result, answer;
 
-        public void nameFrame();
+        public abstract void nameFrame();
 
-        public void generate();
+        public abstract void generate();
 
         public void makeQuizPanel(){
+            nameFrame();
+            generate();
             resetGBC();
+            gbc.insets = new Insets(5,5,5,5);
 
             inputBox = new JTextField();
             inputBox.setPreferredSize(new Dimension(120, 30));
@@ -32,21 +38,61 @@ public class MQ{
             scorePanel = new JPanel(new BorderLayout());
             scorePanel.setBorder(new EmptyBorder(30, 30, 0, 30));
 
+            JLabel positive = new JLabel("Correct: ");
+            positive.setForeground(Color.GREEN);
+            JLabel negative = new JLabel("Incorrect: ");
+            negative.setForeground(Color.RED);
 
-
-
+            scorePanel.add(positive,BorderLayout.WEST);
+            scorePanel.add(negative,BorderLayout.EAST);
 
             quizPanel = new JPanel(new GridBagLayout());
+            mathQuestion = new JLabel("");
+            check = new JButton("Check");
 
+            quizPanel.add(mathQuestion,gbc);
+            gbc.gridy++;
 
+            quizPanel.add(inputBox,gbc);
+            gbc.gridy++;
 
+            quizPanel.add(check,gbc);
 
+            clearFrame();
+            mainFrame.add(scorePanel,BorderLayout.NORTH);
+            mainFrame.add(quizPanel,BorderLayout.CENTER);
+            restartFrame();
+        }
+
+    }
+
+    class Addition extends ArithQ{
+        public void nameFrame(){
+            mainFrame.setTitle("Math Quiz - Addition");
+        }
+        public void generate(){
+            n1 = random.nextInt(51);
+            n2 = random.nextInt(51);
+            result = n1 + n2;
+            mathQuestion.setText(n1 + " + " + n2);
+        }
+
+        public Addition(){
+            makeQuizPanel();
         }
     }
 
     public void resetGBC(){
         gbc.gridx = 0;
         gbc.gridy = 0;
+    }
+
+    public void clearFrame(){
+        mainFrame.getContentPane().removeAll();
+    }
+
+    public void restartFrame(){
+        mainFrame.revalidate();
     }
 
     public void makeMainPanel(){
@@ -62,6 +108,7 @@ public class MQ{
         gbc.gridy++;
 
         addition = new JButton("Addition");
+        addition.addActionListener(e -> new Addition());
         mainPanel.add(addition,gbc);
         gbc.gridy++;
 
